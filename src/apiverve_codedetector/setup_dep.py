fd4d0b48a1fbe143f8301b27bab6856b6880b797
APIVerve Team <hello@apiverve.com>
2025-03-28 17:07:18 -0500

    Bug Fixes

---
+++ src/apiverve_codedetector/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_codedetector',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Code Detector is a simple tool for detecting the language of code in text. It returns details such as extension, language, family, and more.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
